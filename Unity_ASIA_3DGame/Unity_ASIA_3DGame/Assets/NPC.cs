4f29aad8d017baa6de90d994a248341858b47cfd
BoGao123 <71680028+BoGao123@users.noreply.github.com>
2020-12-15 16:05:48 +0800

    寫程式 對話

---
+++ Unity_ASIA_3DGame/Unity_ASIA_3DGame/Assets/NPC.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class NPC : MonoBehaviour
+{
+
+
+    [Header("NPC資料")]
+    public NPCData data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+    public bool playerInArea;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name =="小明")
+        {
+            playerInArea = true;
+            Dialog();
+        }
+
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "小明")
+        {
+            playerInArea = false;
+        }
+
+    }
+
+    private void Dialog()
+    {
+
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        print("我是迴圈:" + i);
+        //    }
+        for (int i = 0; i < data.dialougA.Length; i++)
+        {
+            print(data.dialougA[i]);
+        }
+
+    }
+
+
+}
